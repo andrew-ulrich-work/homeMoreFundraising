e9bab56c7bdd95b2965d230d08f4c000f8733abd
Jordan Walker <jorwalk@gmail.com>
2016-10-22 14:36:37 -0500

    prevention, referral

--- routes/prevention.js
+++ routes/prevention.js
@@ -25,4 +25,32 @@ module.exports = [{
             }
         }
     }
+},{
+    method: 'GET',
+    path: '/prevention/{uuid}',
+    config: {
+        auth: {
+            mode: 'try',
+            strategy: 'session'
+        },
+        plugins: {
+            'hapi-auth-cookie': {
+                redirectTo: false
+            }
+        },
+        handler: function(request, reply) {
+            if (request.auth.isAuthenticated) {
+                var data = {
+                    title: 'This is Index!',
+                    user: request.auth.credentials,
+                    role: request.auth.credentials.type,
+                    prevention: 'active'
+                };
+                request.params.name
+                return reply.view('prevention-single', data, {
+                    layout: 'default'
+                })
+            }
+        }
+    }
 }];
