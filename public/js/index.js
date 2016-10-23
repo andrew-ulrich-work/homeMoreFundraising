68786d0323e1b7a94e78d266f15a932c8811066a
Jordan Walker <jorwalk@gmail.com>
2016-10-22 21:30:16 -0500

    prevention, shelter sign up

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,14 @@
 // Enable popovers everywhere
-$(function () {
-  $('[data-toggle="popover"]').popover()
+$(function() {
+    $('[data-toggle="popover"]').popover()
 })
-
-$('.popover-dismiss').popover({
-  trigger: 'focus'
-})
-
-$('#formNext').on('click',function(){
-	$(this).addClass('hidden-xs-up')
-	$('#fillOut').toggleClass('hidden-xs-up')
+$('document').ready(function() {
+    $('.popover-dismiss').popover({
+        trigger: 'focus'
+    })
+    $('#formNext').on('click', function(e) {
+    	e.preventDefault()
+        $(this).addClass('hidden-xs-up')
+        $('#fillOut').toggleClass('hidden-xs-up')
+    })
 })
