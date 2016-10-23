40166c9ed12e17bf043a30765ed34f58c5eaffb0
Glen Selle <glenselle@me.com>
2016-10-22 21:13:32 -0500

    Latest changes

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,7 @@
 var mongoose = require('mongoose');
 var User = mongoose.model('User');
+var Thread = mongoose.model('Thread');
+var Message = mongoose.model('Message');
 
 module.exports = [{
   method: 'GET',
@@ -17,6 +19,32 @@ module.exports = [{
   handler: function(req, reply) {
     User.find({type: 'client'}).exec(function(err, results) {
       return reply(results);
+    });
+  }
+}, {
+  method: 'GET',
+  path: '/users/{id}/history',
+  config: {
+    auth: {
+      mode: 'try'
+    },
+    plugins: {
+      'hapi-auth-cookie':  {
+        redirectTo: false
+      }
+    }
+  },
+  handler: function(req, reply) {
+    Thread.findOne({client: req.params.id}).exec(function(err, result) {
+      Message
+      .find({thread: result._id})
+      .populate({
+        path: 'author',
+        select: 'name type _id',
+      })
+      .exec(function(err, results) {
+        return reply(results);
+      });
     });
   }
 }]
